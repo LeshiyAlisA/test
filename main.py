33eb907fef6cbb09a7eedbba7f060a9c61ee2dcb
Leshiy <delphi7@list.ru>
2011-04-30 21:05:27 +0400

    test commit

--- main.py
+++ main.py
@@ -1,4 +1,47 @@
 __author__ = 'leshiy'
+
+from xml.dom import minidom
+import urllib
+import sys
 import html5lib
 
-print "OK"
+
+class WebService:
+    def PlaceSingleOrder(self,UserID,PWD,Pair,Expiry,BuySell,Amount,Rate,OrderBasis):
+        print "PlaceSingleOrder"
+
+
+
+    def GetRatesDataSet(self,key):
+        print "GetRates"
+        print key
+        u = urllib.urlopen('http://api.efxnow.com/DEMOWebServices2.8/Service.asmx/GetRatesDataSet?Key='+key)
+        print u.read()
+        dom = html5lib.HTMLParser(u.read(),treebuilder="lxml")
+
+        
+
+
+
+        
+
+
+
+    def GetRatesServerAuth(self,UserID,PWD,Brand):
+        u = urllib.urlopen('http://api.efxnow.com/DEMOWebServices2.8/Service.asmx/GetRatesServerAuth?UserID='+UserID+'&PWD='+PWD+'&Brand='+Brand)
+
+        doc =minidom.parseString(u.read())
+        n=doc.getElementsByTagName("string")[0]
+
+        for nchild in n.childNodes:
+            if nchild.nodeType==3:
+                return nchild.data.strip()
+
+
+
+
+ws=WebService()
+key= ws.GetRatesServerAuth(sys.argv[1],sys.argv[2],sys.argv[3])
+ws.GetRatesDataSet(key)
+
+
