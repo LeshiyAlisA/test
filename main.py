6d8cc47fec32ad3f81db8cdf93c75fa50a4d0dcf
leshiy <delphi7@list.ru>
2011-05-03 17:38:55 +0400

    удалил лишнее

--- main.py
+++ main.py
@@ -1,32 +1,27 @@
 __author__ = 'leshiy'
+#-*- coding: utf-8 -*-
 
 from xml.dom import minidom
 import urllib
 import sys
 import html5lib
+from html5lib import treebuilders
+from lxml import etree
+
+
+
+
 
 
 class WebService:
     def PlaceSingleOrder(self,UserID,PWD,Pair,Expiry,BuySell,Amount,Rate,OrderBasis):
         print "PlaceSingleOrder"
 
-
-
     def GetRatesDataSet(self,key):
         print "GetRates"
         print key
         u = urllib.urlopen('http://api.efxnow.com/DEMOWebServices2.8/Service.asmx/GetRatesDataSet?Key='+key)
-        print u.read()
-        dom = html5lib.HTMLParser(u.read(),treebuilder="lxml")
-
         
-
-
-
-        
-
-
-
     def GetRatesServerAuth(self,UserID,PWD,Brand):
         u = urllib.urlopen('http://api.efxnow.com/DEMOWebServices2.8/Service.asmx/GetRatesServerAuth?UserID='+UserID+'&PWD='+PWD+'&Brand='+Brand)
 
