917c6ab37da3bdbaa0d52e4fc17b07fce630128a
leshiy <delphi7@list.ru>
2011-05-13 13:59:28 +0400

    запись в базу

--- main.py
+++ main.py
@@ -5,20 +5,54 @@ import forex
 from pymongo import Connection
 import sys
 import pickle
-connection=Connection("localhost",27017)
-db=connection.db
-rates=db.rates
 
+class main:
+
+    def analiz(self,Rate):
+
+        print Rate
+
+    def test(self):
+
+        vs=forex.WebService()
+        f = open('/home/leshiy/str.txt', 'rb+')
+        y=pickle.load(f)
+        key=vs.GetRatesServerAuth(y[0],y[1],y[2])
+
+        while 1==1:
+            Rates=vs.GetRatesDataSet(key)
+            if Rates!=0:
+                for i in Rates:
+                    if i["Quote"]=="EUR/USD":
+                        self.analiz(i)
+
+
+
+    def test2(self):
+        connection=Connection("localhost",27017)
+        db=connection.db
+        rates=db.rates
+
+        vs=forex.WebService()
+        f = open('/home/leshiy/str.txt', 'rb+')
+        y=pickle.load(f)
+        key=vs.GetRatesServerAuth(y[0],y[1],y[2])
+
+        while 1==1:
+            Rates=vs.GetRatesDataSet(key)
+            for i in Rates:
+                rates.save(i)
+                print rates.count()
+
+        
 vs=forex.WebService()
 f = open('/home/leshiy/str.txt', 'rb+')
 y=pickle.load(f)
 key=vs.GetRatesServerAuth(y[0],y[1],y[2])
+vs.GetRatesBlotter(key)
 
-while 1==1:
-    Rates=vs.GetRatesDataSet(key)
-    for i in Rates:
-        rates.save(i)
-        print rates.count()
+
+  
 
 
 
